614e89bb5ba6ce18b742f0014bc1808c147b94c1
milkyklim <klim.kolyvanov@gmail.com>
2018-09-05 16:50:24 +0200

    updated the way we create the candlesticks

--- candleSchema.js
+++ candleSchema.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+const fs = require('fs');
+
 const mongoose = require('mongoose');
 // schema is used to write the data to the database
 const Schema = mongoose.Schema;
@@ -14,12 +17,33 @@ const candleSchema = new Schema({
 
 // TODO: check if this is actually the best way to write the same
 // datastructure to different collections 
-const btcCandle = mongoose.model('btcCandle', candleSchema);
-const poaCandle = mongoose.model('poaCandle', candleSchema);
-const ontCandle = mongoose.model('ontCandle', candleSchema);
+// TODO: maybe it makes sense to have some time of dictionary here? 
+// btc -> model1, poa -> model2, ont -> model3 
+// const btcCandle = mongoose.model('btcCandle', candleSchema);
+// const poaCandle = mongoose.model('poaCandle', candleSchema);
+// const ontCandle = mongoose.model('ontCandle', candleSchema);
+
+// module.exports = {
+//   btcCandle, 
+//   poaCandle, 
+//   ontCandle
+// };
+
+// read the file btc-market-tickers.txt 
+// and create the corresponding models 
+const models = {};
+// sync read 
+fs.readFileSync(process.env.BTC_MARKETS_FILE).toString().split('\n').forEach((ticker) => { 
+    console.log(ticker); 
+    // dict[line] = 'model' + (idx++);
+    models[ticker] = mongoose.model(ticker, candleSchema);
+})
+console.log(models);
+
+// general construct should look like 
+// const ontCandle = mongoose.model(model1, candleSchema);
+// models['ETHBTC'] = mongoose.model('ETHBTC', candleSchema);
 
 module.exports = {
-  btcCandle, 
-  poaCandle, 
-  ontCandle
-};
+  models
+}
