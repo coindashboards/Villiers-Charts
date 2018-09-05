6f563b4d40c44a19a77c72f19fbe61585e40ec64
milkyklim <klim.kolyvanov@gmail.com>
2018-09-05 19:04:44 +0200

    mvp that grabs 1m minute candlesticks and saves them to the db

--- app.js
+++ app.js
@@ -16,35 +16,45 @@ const Candle = require('./candleSchema');
 let tokens = Object.keys(Candle.models);
 
 binance.websockets.candlesticks(tokens, "1m", (candlesticks) => {
-  let { e:eventType, E:eventTime, s:symbol, k:ticks } = candlesticks;
-  let { o:open, h:high, l:low, c:close, v:volume, n:trades, i:interval, x:isFinal, q:quoteVolume, V:buyVolume, Q:quoteBuyVolume } = ticks;
-  if (isFinal == true){
-    console.log(symbol+" "+interval+" candlestick update");
-    console.log("open: "+open);
-    console.log("high: "+high);
-    console.log("low: "+low);
-    console.log("close: "+close);
-    console.log("volume: "+volume);
-    console.log("isFinal: "+isFinal);
+  let { e:eventType, E:eventTime, s:pSymbol, k:pTicks } = candlesticks;
+  let { o:pOpen, h:pHigh, l:pLow, c:pClose, v:pVolume, n:pTrades, i:pInterval, x:pIsFinal, q:pQuoteVolume, V:pBuyVolume, Q:pQuoteBuyVolume } = pTicks;
+  if (pIsFinal == true){
+    // saving part
+    let candle = new Candle.models[pSymbol]({
+      // time: {type: Date},
+      open: pOpen,
+      close: pClose,
+      high: pHigh,
+      low: pLow,
+      numTrades: pTrades,
+      volume: pVolume
+    });
+
+    candle.save((err) => {
+      if (err) {
+        // TODO: propagate error handling
+        console.log('Error:(');
+      }
+      else{
+        // console.log('DOGE!');
+      }
+    });
+
+    debugOutput(candlesticks);
   }
 });
 
+const debugOutput = function(candlestick){
+  let { e:eventType, E:eventTime, s:symbol, k:ticks } = candlestick;
+  let { o:open, h:high, l:low, c:close, v:volume, n:trades, i:interval, x:isFinal, q:quoteVolume, V:buyVolume, Q:quoteBuyVolume } = ticks;
 
-// saving part
-// let candle = new Candle.models['POABTC']({
-//   // time: {type: Date},
-//   open: 42,
-//   close: 42,
-//   high: 42,
-//   low: 8,
-//   numTrades: 11,
-//   volume: 34.5
-// });
-// candle.save((err) => {
-//   if (err) 
-//     console.log('Error:(');
-//   else
-//     console.log('DOGE!');
-// });
+  console.log(symbol+" "+interval+" candlestick update");
+  console.log("open: " + open);
+  console.log("high: " + high);
+  console.log("low: " + low);
+  console.log("close: " + close);
+  console.log("volume: " + volume);
+  console.log("isFinal: " + isFinal);
+}
 
 console.log('DOGE!');
