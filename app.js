da4fa8d71c1ee337fc07d81375ac8db7539db504
milkyklim <klim.kolyvanov@gmail.com>
2018-09-05 17:47:07 +0200

    minimal working example of candlesticks and models

--- app.js
+++ app.js
@@ -13,13 +13,12 @@ mongoose.connect(process.env.MONGO_DB_URL, {useNewUrlParser: true}, (err) => {
 });
 
 const Candle = require('./candleSchema');
+let tokens = Object.keys(Candle.models);
 
-let tokens = new Map([ ["POA", Candle.poaCandle], ["ONT", Candle.ontCandle]]);
-
-binance.websockets.candlesticks(['BNBBTC', 'ONTETH', 'ETHBTC', 'LTCBTC','EOSBTC'], "1m", (candlesticks) => {
+binance.websockets.candlesticks(tokens, "1m", (candlesticks) => {
   let { e:eventType, E:eventTime, s:symbol, k:ticks } = candlesticks;
   let { o:open, h:high, l:low, c:close, v:volume, n:trades, i:interval, x:isFinal, q:quoteVolume, V:buyVolume, Q:quoteBuyVolume } = ticks;
-  if (isFinal == false){
+  if (isFinal == true){
     console.log(symbol+" "+interval+" candlestick update");
     console.log("open: "+open);
     console.log("high: "+high);
@@ -32,11 +31,11 @@ binance.websockets.candlesticks(['BNBBTC', 'ONTETH', 'ETHBTC', 'LTCBTC','EOSBTC'
 
 
 // saving part
-// let candle = new Candle.btcCandle({
+// let candle = new Candle.models['POABTC']({
 //   // time: {type: Date},
-//   open: 12,
-//   close: 15,
-//   high: 25,
+//   open: 42,
+//   close: 42,
+//   high: 42,
 //   low: 8,
 //   numTrades: 11,
 //   volume: 34.5
--- candleSchema.js
+++ candleSchema.js
@@ -15,34 +15,11 @@ const candleSchema = new Schema({
   volume: {type: Number},
 });
 
-// TODO: check if this is actually the best way to write the same
-// datastructure to different collections 
-// TODO: maybe it makes sense to have some time of dictionary here? 
-// btc -> model1, poa -> model2, ont -> model3 
-// const btcCandle = mongoose.model('btcCandle', candleSchema);
-// const poaCandle = mongoose.model('poaCandle', candleSchema);
-// const ontCandle = mongoose.model('ontCandle', candleSchema);
-
-// module.exports = {
-//   btcCandle, 
-//   poaCandle, 
-//   ontCandle
-// };
-
-// read the file btc-market-tickers.txt 
-// and create the corresponding models 
 const models = {};
 // sync read 
 fs.readFileSync(process.env.BTC_MARKETS_FILE).toString().split('\n').forEach((ticker) => { 
-    console.log(ticker); 
-    // dict[line] = 'model' + (idx++);
-    models[ticker] = mongoose.model(ticker, candleSchema);
+    models[ticker] = mongoose.model(ticker, candleSchema, ticker); // second ticker: name of the collection 
 })
-console.log(models);
-
-// general construct should look like 
-// const ontCandle = mongoose.model(model1, candleSchema);
-// models['ETHBTC'] = mongoose.model('ETHBTC', candleSchema);
 
 module.exports = {
   models
