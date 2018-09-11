1064532c3143d3c27bb0ebc0e02d310325142a08
milkyklim <klim.kolyvanov@gmail.com>
2018-09-11 23:38:56 +0200

    added correct expiration date

--- src/constants.js
+++ src/constants.js
@@ -3,8 +3,7 @@ module.exports = {
     TIME_40_MINUTES: 40*60*1000, 
     TIME_8_HOURS: 8*60*60*1000,
     TIME_7_DAYS: 7*24*60*60*1000,
-
-    EXPIRATION_TIME_SECONDS: 3 * 60, // 7*24*60*60*1000 + 60*60*1000, // 7 days + 1 hour
+    EXPIRATION_TIME_SECONDS: 7*24*60*60*1000 + 60*60*1000, // 7 days + 1 hour
     MONGOBD_POOLSIZE: 50,
     AUTHOR : 'milkyklim'
 }
