7fdc0339edf6e1bcf4d081cbfa57a9c7e8dd927c
milkyklim <klim.kolyvanov@gmail.com>
2018-09-12 23:54:49 +0200

    app.js now looks as it should; setup.js has some comments now

--- app.js
+++ app.js
@@ -2,38 +2,16 @@
 require('dotenv').config();
 // TODO: do I need to connect here or somewhere else?
 const mongoose = require('mongoose');
-const binance = require('node-binance-api')();
-
-const utils = require('./utils/utils.js');
-const debugOutput = utils.debugOutput;
-const createCandle = utils.createCandle;
 
 // TODO: check if this is necessary at all
 mongoose.Promise = global.Promise;
-const mongodbConnModule = require('./src/mongodbConnModule.js');
-const db = mongodbConnModule.connect();
+const mongodbConnModule = require('./src/database');
+mongodbConnModule.connect();
 
-const Candle = require('./models/candleSchema.js');
-let tokens = Object.keys(Candle.models);
+// grab the candlesticks from binance
+require('./src/candlesticks');
 
-// const Heatmap = require('./models/heatmapSchema.js');
+// create the heatmaps 
+// require('./src/heatmaps');
 
-const getCandlesticks = function(tokens, p){
-  binance.websockets.candlesticks(tokens, p, (candlestick) => {
-    // k == ticks, x == isFinal
-    let isFinal = candlestick.k.x;
-    if (isFinal == true){
-        let candle = createCandle(candlestick, p);
-        candle.save()
-        .catch((err) => console.log(err))
-        .then(() => debugOutput(candlestick));
-      }
-  });
-}
-
-const timeframes = ['1m', '5m', '1h', '1d'];
-timeframes.forEach((p) => {
-  getCandlesticks(tokens, p);
-});
-
-console.log('DOGE!');
+console.log('app started!');
--- utils/setup.js
+++ utils/setup.js
@@ -1,16 +1,13 @@
+/**
+ * grad all btc markets and save them in the file
+ * TODO: write the markets to the database?
+ */
+
 // TODO: need this one ?
 require('dotenv').config();
 
 const binance = require('node-binance-api')();
 const fs = require('fs');
-
-binance.prices((error, tickers) => {
-    // save all btc tickers
-    let btcTickers =  filterMarkets(tickers, 'BTC');
-    console.log('Got all btc tickers');
-    // dump all btc tickers to the file
-    dumpTickers(btcTickers, BTC_MARKETS_FILE);
-});
 
 const filterMarkets = function(tickers, token){
     let btcTickers = [];
@@ -31,3 +28,11 @@ const dumpTickers = function(tickers, filename){
     tickers.forEach(v => fileStream.write(v  + '\n'));
     fileStream.end();
 }
+
+binance.prices((error, tickers) => {
+    // save all btc tickers
+    let btcTickers =  filterMarkets(tickers, 'BTC');
+    console.log('Got all btc tickers');
+    // dump all btc tickers to the file
+    dumpTickers(btcTickers, BTC_MARKETS_FILE);
+});
