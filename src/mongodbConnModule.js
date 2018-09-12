eb457b66e7a3780c1883172cf0652522e01c1172
milkyklim <klim.kolyvanov@gmail.com>
2018-09-12 12:54:11 +0200

    polishing

--- src/mongodbConnModule.js
+++ src/mongodbConnModule.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports.connect = function() {
 	mongoose.connect(process.env.MONGO_DB_URL, {useNewUrlParser: true, poolSize: 50});
-	mongoose.set('useCreateIndex', true)
+	mongoose.set('useCreateIndex', true);
 	let db = mongoose.connection;
 	db.on("error", console.error.bind(console, "connection error"));
 	db.once("open", (callback) => {
