bb5850cd6af4e7ceb26c9606eb601bca100def63
milkyklim <klim.kolyvanov@gmail.com>
2018-09-15 14:51:34 +0200

    update time should be 5 mins not 20 mins

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 module.exports = {
-    UPDATE_TIME_INTERVAL : 20 * 60 * 1000,
+    UPDATE_TIME_INTERVAL : 5 * 60 * 1000,
     DELTA_TIME_BETWEEN_CANDLES : 60 * 1000,
-    TIME_40_MINUTES: 40 * 60 * 1000, 
+    TIME_40_MINUTES: 40 * 60 * 1000,
     TIME_8_HOURS: 8 * 60 * 60 * 1000,
     TIME_7_DAYS: 7 * 24 * 60 * 60 * 1000,
     EXPIRATION_TIME_SECONDS: 7 * 24 * 60 * 60 * 1000 + 60 * 60 * 1000, // 7 days + 1 hour
