1eba41a7fe580c51d96a9138ac0e995e309c2da0
milkyklim <klim.kolyvanov@gmail.com>
2018-09-17 07:53:48 +0200

    30 seconds constant

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,7 @@
 module.exports = {
     UPDATE_TIME_INTERVAL : 5 * 60 * 1000,
     DELTA_TIME_BETWEEN_CANDLES : 60 * 1000,
+    TIME_30_SECONDS: 30 * 1000,
     TIME_40_MINUTES: 40 * 60 * 1000,
     TIME_8_HOURS: 8 * 60 * 60 * 1000,
     TIME_7_DAYS: 7 * 24 * 60 * 60 * 1000,
