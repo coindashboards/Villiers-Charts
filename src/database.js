3cb02c8cc47e6e23efbc15bef281fd8f554ab469
milkyklim <klim.kolyvanov@gmail.com>
2018-09-19 11:41:56 +0200

    use constant where necessary

--- src/database.js
+++ src/database.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+const constants = require('constants');
 
 module.exports.connect = function() {
-	mongoose.connect(process.env.MONGO_DB_URL, {useNewUrlParser: true, poolSize: 50});
+	mongoose.connect(process.env.MONGO_DB_URL, {useNewUrlParser: true, poolSize: constants.MONGOBD_POOLSIZE});
 	mongoose.set('useCreateIndex', true);
 	let db = mongoose.connection;
 	db.on("error", console.error.bind(console, "Connection error"));
